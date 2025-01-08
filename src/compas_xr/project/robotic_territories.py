979e5177b46cfc25bbb8173b0ba89fdbb5cc0c58
Joseph Kenny <jckenny59@gmail.com>
2025-01-07 19:03:17 -0500

    update uploads

--- src/compas_xr/project/robotic_territories.py
+++ src/compas_xr/project/robotic_territories.py
@@ -6,14 +6,19 @@ from compas.datastructures import Part
 from compas.geometry import Frame
 from compas.geometry import Point
 from compas.geometry import Transformation
-from compas_rhino.conversions import plane_to_compas_frame, frame_to_rhino_plane
+from compas_rhino.conversions import plane_to_compas_frame, frame_to_rhino_plane, box_to_compas, point_to_compas
 from compas.geometry import Translation
 from compas.geometry import Box
 from Rhino.Geometry import Point3d
+import Rhino.Geometry as rg
 
 from copy import deepcopy
 import math
 
+from compas_xr.project.assembly_extensions import AssemblyExtensions
+from compas_xr.realtime_database import RealtimeDatabase
+from compas_xr.storage import Storage
+from compas_xr.project.project_manager import ProjectManager
 
 class OptiTrackConversions(object):
 
@@ -148,5 +153,98 @@ class OptitrackItems(object):
 
         return box
     
+class RoboticTerritoriesFormattingHelpers(object):
+
+    def format_box_dict(self, rhino_box):
+        """
+        Converts a Rhino Box to a compas Box for returning required dictionary.
+        
+        Parameters:
+        - box: Rhino.Geometry.Box, the input box.
     
+        Returns:
+        - compas box data (Compas.Geometry.Box.__data__).
+        """
+        if not isinstance(rhino_box, rg.Box):
+            raise TypeError("The input is not a Rhino Box.")
+        
+        else:
+            compas_box = box_to_compas(rhino_box)
+            dict = compas_box.__data__
+            return dict
+
+    def format_polyline_to_compas_points(self, polyline):
+        """
+        Extract points from a Rhino.Geometry.Polyline.
+    
+        Parameters:
+        - polyline: Rhino.Geometry.Polyline, the input polyline.
+    
+        Returns:
+        - List of points (Rhino.Geometry.Point3d).
+        """
+        if isinstance(polyline, Polyline):  # Ensure it's a Polyline
+            points = [point for point in polyline]
+            compas_points = [point_to_compas(pt) for pt in points]
+            points_dict = {index: pt.__data__ for index, pt in enumerate(compas_points)}
+            print (points_dict)
+            return points_dict
+
+        else:
+            raise TypeError("The input is not a Polyline.")
+            
+class RoboticTerritoriesProjectManager(object):
+    # """
+    # The ProjectManager class is responsible for managing project specific data and operations that involve
+    # Firebase Storage and Realtime Database configuration.
+
+    # Parameters
+    # ----------
+    # config_path : str
+    #     The path to the configuration file for the project.
+
+    # Attributes
+    # ----------
+    # storage : Storage
+    #     The storage instance for the project.
+    # database : RealtimeDatabase
+    #     The realtime database instance for the project.
+    # """
+
+    def __init__(self, config_path):
+        if not os.path.exists(config_path):
+            raise Exception("Could not create Storage or Database with path {}!".format(config_path))
+        self.storage = Storage(config_path)
+        self.database = RealtimeDatabase(config_path)
+        self.project_manager = ProjectManager(config_path)
+        self.FormatHelpers = RoboticTerritoriesFormattingHelpers()
+
+    
+    def upload_rt_project_data(self, project_name, qr_frames, boundary_zone, infer_pick_zone, infer_collab_zone, mimic_human_zone, mimic_robot_zone, tele_mimic_zone):
+        
+        upload_dict = {}
+        
+        boundary_dict = self.FormatHelpers.format_box_dict(boundary_zone)
+        upload_dict["boundry_zone"] = boundary_dict
+        
+        infer_dict = {}
+        infer_pick_dict = self.FormatHelpers.format_box_dict(infer_pick_zone)
+        infer_collab_dict = self.FormatHelpers.format_box_dict(infer_collab_zone)
+        infer_dict["collaboration_zone"] = infer_collab_dict
+        infer_dict["pick_zone"] = infer_pick_dict
+        upload_dict["inferance_zones"] = infer_dict
+        
+        mimic_dict = {}
+        mimic_human_dict = self.FormatHelpers.format_box_dict(mimic_human_zone)
+        mimic_robot_dict = self.FormatHelpers.format_box_dict(mimic_robot_zone)
+        mimic_dict["human_zone"] = mimic_human_dict
+        mimic_dict["robot_zone"] = mimic_robot_dict
+        upload_dict["mimic_zones"] = mimic_dict
+        
+        tele_mimic_dict = self.FormatHelpers.format_box_dict(tele_mimic_zone)
+        upload_dict["tele_mimic_zone"] = tele_mimic_dict
+        
+        self.database.upload_data_to_reference_as_child(upload_dict, project_name, "zones")
+        self.project_manager.upload_qr_frames_to_project(project_name, qr_frames)
+        
 
