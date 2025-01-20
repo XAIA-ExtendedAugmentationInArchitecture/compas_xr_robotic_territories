39821979b08cfa868bd30b1481b424e93c2d020b
Joseph Kenny <jckenny59@gmail.com>
2025-01-20 11:06:58 -0500

    update pybullet simulation to load the robot from the repo
    fix file path to the added urdf file from the repo, so it should work for everyone simultaneously.

--- dev/python/pybullet_simmulation_test.py
+++ dev/python/pybullet_simmulation_test.py
@@ -1,11 +1,17 @@
+import os
 import time
+
 import compas_fab
 from compas_fab.backends import PyBulletClient
 from compas_fab.robots import CollisionMesh
 
-with PyBulletClient() as client:
-    urdf_path = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\02_Production\01_SetUp\20250105_setup_working\ur_description\urdf\ur20.urdf"
-    urdf_filepath = compas_fab.get(urdf_path)
-    robot = client.load_robot(urdf_filepath)
+# Construct file path to urdf file
+script_dir = os.path.dirname(os.path.abspath(__file__))
+urdf_relative_path = os.path.join(script_dir, "../scripts/urdf/ur_description/urdf/ur20.urdf")
+urdf_file_path = os.path.normpath(urdf_relative_path)
 
+# Load robot in PyBullet
+with PyBulletClient() as client:
+    urdf_file = compas_fab.get(urdf_file_path)
+    robot = client.load_robot(urdf_file)
     time.sleep(15)
