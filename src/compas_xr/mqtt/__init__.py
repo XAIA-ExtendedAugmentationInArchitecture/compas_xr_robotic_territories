71c00348cc5e71065147cad92c6356ed16fbb186
Joseph Kenny <jckenny59@gmail.com>
2025-01-23 21:28:56 -0500

    update message classes and parsing for execution

--- src/compas_xr/mqtt/__init__.py
+++ src/compas_xr/mqtt/__init__.py
@@ -22,6 +22,6 @@ MQTT Messages
 """
 
 from .messages import ApproveTrajectory, GetTrajectoryRequest, GetTrajectoryResult, SendTrajectory
-from.robotic_territories_messages import MimicTrajectoryRequestMessage, MimicTrajectoryResultMessage
+from.robotic_territories_messages import MimicTrajectoryRequestMessage, MimicTrajectoryResultMessage, ExecuteMimicTrajectoryRequestMessage
 
-__all__ = ["GetTrajectoryRequest", "GetTrajectoryResult", "ApproveTrajectory", "SendTrajectory", "MimicTrajectoryRequestMessage", "MimicTrajectoryResultMessage"]
+__all__ = ["GetTrajectoryRequest", "GetTrajectoryResult", "ApproveTrajectory", "SendTrajectory", "MimicTrajectoryRequestMessage", "MimicTrajectoryResultMessage", "ExecuteMimicTrajectoryRequestMessage"]
--- src/compas_xr/mqtt/robotic_territories_messages.py
+++ src/compas_xr/mqtt/robotic_territories_messages.py
@@ -10,18 +10,30 @@ from compas_eve import Message
 
 class MessageHandelingExtensions(object):
 
-    def _parse_frames_list_from_data(self, data):
+    @staticmethod
+    def _parse_frames_list_from_data(data):
         """Parse the list of frames from the input data."""
         return [Frame.__from_data__(frame) for frame in data] #TODO: CHECK IF THIS WORKS... it does :)
     
-    def _parse_trajectory_list(self, data):
+    @staticmethod
+    def _parse_trajectory_list(data):
         """Parse the list of trajectories from the input data."""
         #TODO: Check this and throw an error?
-        return [JointTrajectory.__from_data__(trajectory) for trajectory in data]
+        for trajectory in data:
+            # print(trajectory)
+            traj = JointTrajectory.__from_data__(trajectory)
+
+        # return [JointTrajectory.__from_data__(trajectory) for trajectory in data]
     
-    def _parse_trajectory_point_list(self, data):
+    @staticmethod #TODO: I am not sure why __from_data__ did not work...
+    def _parse_trajectory_point_list(data):
         """Parse the list of trajectory points from the input data."""
-        return [JointTrajectoryPoint.__from_data__(point) for point in data]
+        for point in data:
+            pt = JointTrajectoryPoint(point["joint_values"], point["joint_types"], point["joint_names"])
+            pt.accelerations = point["accelerations"]
+            pt.effort = point["effort"]
+            pt.velocities = point["velocities"]
+        return pt
 
 
 class Header(Message):
@@ -161,33 +173,36 @@ class MimicTrajectoryResultMessage(Message):
         robot_name = value["robot_name"]
         return cls(trajectories, robot_base_frame, robot_name, header)
 
-    # class ExecuteMimicTrajectoryRequestMessage(Message):
-    #     """
-    #     The ExecuteMimicTrajectoryRequestMessage class is responsible for requesting a robot to execute a trajectory.
+class ExecuteMimicTrajectoryRequestMessage(Message):
 
-    #     The ExecuteMimicTrajectoryRequestMessage class provides methods for parsing, updating, and accessing the fields of a message,
-    #     and provides a means of defining attributes of the message in order to accept or ignore specific messages.
+        """
+        The ExecuteMimicTrajectoryRequestMessage class is responsible for requesting a robot to execute a trajectory.
 
-    #     Parameters
-    #     ----------
-    #     header : Header
-    #         The header of the message.
-    #     """
+        The ExecuteMimicTrajectoryRequestMessage class provides methods for parsing, updating, and accessing the fields of a message,
+        and provides a means of defining attributes of the message in order to accept or ignore specific messages.
 
-    #     def __init__(self, trajectories, combined_trajectory_points_list, robot_name, robot_base_frame, header=None):
-    #         super(ExecuteMimicTrajectoryRequestMessage, self).__init__()
-    #         self["header"] = header or Header()
-    #         self["trajectories"] = trajectories
-    #         self["robot_name"] = robot_name
-    #         self["robot_base_frame"] = robot_base_frame
-    #         self["combined_trajectory_points_list"] = combined_trajectory_points_list
+        Parameters
+        ----------
+        header : Header
+            The header of the message.
+        """
 
-    #     @classmethod
-    #     def parse(cls, value):
-    #         """Parse the message information
-    #         from the input value
-    #         """
-    #         header = Header.parse(value["header"])
-    #         trajectory = JointTrajectory.__from_data__(value["trajectory"])
-    #         robot_name = value["robot_name"]
-    #         return cls(trajectory, robot_name, header)
+        def __init__(self, trajectories, combined_trajectory_points_list, robot_name, robot_base_frame, header=None):
+            super(ExecuteMimicTrajectoryRequestMessage, self).__init__()
+            self["header"] = header or Header()
+            self["trajectories"] = trajectories
+            self["robot_name"] = robot_name
+            self["robot_base_frame"] = robot_base_frame
+            self["combined_trajectory_points"] = combined_trajectory_points_list
+
+        @classmethod
+        def parse(cls, value):
+            """Parse the message information
+            from the input value
+            """
+            header = Header.parse(value["header"])
+            robot_name = value["robot_name"]
+            robot_base_frame = Frame.__from_data__(value["robot_base_frame"])
+            trajectories = value["trajectories"]
+            combined_trajectory_points_list = MessageHandelingExtensions._parse_trajectory_point_list(value["combined_trajectory_points"])
+            return cls(trajectories, combined_trajectory_points_list, robot_name, robot_base_frame, header)
