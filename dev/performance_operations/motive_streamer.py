e4a8bb3d407f4129ccd7b4891656e335287d5413
Joseph Kenny <jckenny59@gmail.com>
2025-09-13 17:43:38 -0400

    update random information and remove fp

--- dev/performance_operations/motive_streamer.py
+++ dev/performance_operations/motive_streamer.py
@@ -96,7 +96,7 @@ geometry_manager = GeometryManager(
     BOX_SIZE[0],
     BOX_SIZE[1],
     BOX_SIZE[2],
-    firebase_upload=False
+    firebase_upload=UPLOAD_TO_FIREBASE
 )
 
 # Storage Directories file names and paths
