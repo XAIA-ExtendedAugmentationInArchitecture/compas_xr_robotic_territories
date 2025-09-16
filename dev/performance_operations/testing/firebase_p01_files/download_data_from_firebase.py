edb855c14ffb3e2ef62fbe6f18b60e54573ea2dc
Joseph Kenny <jckenny59@gmail.com>
2025-09-16 11:11:07 -0400

    testing and uploading of performance data

--- dev/performance_operations/testing/firebase_p01_files/download_data_from_firebase.py
+++ dev/performance_operations/testing/firebase_p01_files/download_data_from_firebase.py
@@ -1,23 +1,25 @@
 from compas_xr.realtime_database import RealtimeDatabase
 from compas.data import json_load
 import os
+import time
+from compas.data import json_dump
 
 
 if __name__ == "__main__":
 
     # project_name = "robotic_territories_general_inference_testing"
     project_name = "rt_larget_zones_expiriment_test"
-    project_config_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\project_config.json"
-    project_config_dict = json_load(project_config_fp)
-    fb_db_config_fp = project_config_dict.get("firebase_config_fp", None)
-    if not fb_db_config_fp:
-        raise ValueError("No 'firebase_config_fp' found in project_config.json")
+    # project_config_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\project_config.json"
+    # project_config_dict = json_load(project_config_fp)
+    # fb_db_config_fp = project_config_dict.get("firebase_config_fp", None)
+    # if not fb_db_config_fp:
+    #     raise ValueError("No 'firebase_config_fp' found in project_config.json")
 
-    import os, pprint
-    folder = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing"
-    print("Folder exists:", os.path.isdir(folder))
-    pprint.pprint([repr(x) for x in os.listdir(folder)])
-
+    # import os, pprint
+    # folder = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing"
+    # print("Folder exists:", os.path.isdir(folder))
+    # pprint.pprint([repr(x) for x in os.listdir(folder)])
+    fb_db_config_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\fb_config\robotic_territories_fb.json"
     rt_db = RealtimeDatabase(fb_db_config_fp)
     # data_to_upload_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing\robotic-territories-default-rtdb-robotic_territories_testing_base_frame-export.json"
     # data_to_upload_fp = r"\\?\C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing\robotic-territories-default-rtdb-robotic_territories_testing_base_frame-export.json"
@@ -32,4 +34,18 @@ if __name__ == "__main__":
         raise FileNotFoundError(f"Data file not found: {data_to_upload_fp}")
     # data = json_load(data_to_upload_fp)
 
-    rt_db.upload_data_from_file(path_local=data_to_upload_fp, refernce_name=project_name)
+    # rt_db.upload_data_from_file(path_local=data_to_upload_fp, refernce_name=project_name)
+
+    start = time.time()
+    data = rt_db.get_data(reference_name=project_name)
+    save_folder = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\firebase_p01_files"
+    file_name = f"rt_larget_zones_expiriment_test_downloaded_BASE.json"
+    save_fp = os.path.join(save_folder, file_name)
+    json_dump(fp=save_fp, data=data, pretty=True)
+    end = time.time()
+    print(f"type of data retrieved: {type(data)}")
+    print(f"Retrieved data keys: {list(data.keys())}")
+    print(f"Data retrieval took {end - start:.4f} seconds.")
+    # TODO: Example : Data retrieval took 0.1171 seconds.
+
+
--- dev/performance_operations/testing/firebase_p01_files/upload_data_to_firebase.py
+++ dev/performance_operations/testing/firebase_p01_files/upload_data_to_firebase.py
@@ -1,24 +1,23 @@
 from compas_xr.realtime_database import RealtimeDatabase
 from compas.data import json_load
 import os
-import time
 
 
 if __name__ == "__main__":
 
     # project_name = "robotic_territories_general_inference_testing"
-    project_name = "rt_larget_zones_expiriment_test"
-    project_config_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\project_config.json"
-    project_config_dict = json_load(project_config_fp)
-    fb_db_config_fp = project_config_dict.get("firebase_config_fp", None)
-    if not fb_db_config_fp:
-        raise ValueError("No 'firebase_config_fp' found in project_config.json")
+    project_name = "rt_qr_frames_test"
+    # project_config_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\project_config.json"
+    # project_config_dict = json_load(project_config_fp)
+    # fb_db_config_fp = project_config_dict.get("firebase_config_fp", None)
+    # if not fb_db_config_fp:
+    #     raise ValueError("No 'firebase_config_fp' found in project_config.json")
 
-    import os, pprint
-    folder = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing"
-    print("Folder exists:", os.path.isdir(folder))
-    pprint.pprint([repr(x) for x in os.listdir(folder)])
-
+    # import os, pprint
+    # folder = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing"
+    # print("Folder exists:", os.path.isdir(folder))
+    # pprint.pprint([repr(x) for x in os.listdir(folder)])
+    fb_db_config_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\fb_config\robotic_territories_fb.json"
     rt_db = RealtimeDatabase(fb_db_config_fp)
     # data_to_upload_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing\robotic-territories-default-rtdb-robotic_territories_testing_base_frame-export.json"
     # data_to_upload_fp = r"\\?\C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing\robotic-territories-default-rtdb-robotic_territories_testing_base_frame-export.json"
@@ -27,20 +26,11 @@ if __name__ == "__main__":
     # data_to_upload_fp = r"C:\Users\jk6372\Downloads\robotic-territories-telemimic_general_working_again.json"
     # data_to_upload_fp = r"C:\Users\jk6372\Downloads\rt_inference_upload_again.json"
     # data_to_upload_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing\rt_inference_upload_again_larger_zones_expiriment_test.json"
-    data_to_upload_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing\rt_inference_upload_again_larger_zones_expiriment_test_actual.json"
+    # data_to_upload_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\fire_base_geo_upload_and_tracking_testing\rt_inference_upload_again_larger_zones_expiriment_test_actual.json"
+    data_to_upload_fp = r"C:\Users\jk6372\Desktop\00_princeton_projects\00_robotic_territories\00_git\compas_xr_robotic_territories\dev\performance_operations\testing\firebase_p01_files\rt_larget_zones_equal_size.json"
 
     if not os.path.exists(data_to_upload_fp):
         raise FileNotFoundError(f"Data file not found: {data_to_upload_fp}")
     # data = json_load(data_to_upload_fp)
 
-    # rt_db.upload_data_from_file(path_local=data_to_upload_fp, refernce_name=project_name)
-
-    start = time.time()
-    data = rt_db.get_data(reference_name=project_name)
-    end = time.time()
-    print(f"type of data retrieved: {type(data)}")
-    print(f"Retrieved data keys: {list(data.keys())}")
-    print(f"Data retrieval took {end - start:.4f} seconds.")
-    # TODO: Example : Data retrieval took 0.1171 seconds.
-
-
+    rt_db.upload_data_from_file(path_local=data_to_upload_fp, refernce_name=project_name)
