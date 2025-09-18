6afdf15acbfa981fc7a5b231ec56daf3d2ff5e98
Joseph Kenny <jckenny59@gmail.com>
2025-09-18 00:37:50 -0400

    checking from chaosnet

--- dev/performance_operations/communication_handler_updated_TOFINISH.py
+++ dev/performance_operations/communication_handler_updated_TOFINISH.py
@@ -866,7 +866,8 @@ ROBOT_NAME = "UR20"
 
 # MQTT_CONFIG = PROJECT_CONFIG_DICT["mqtt_config"]
 # MQTT_CONFIG = PROJECT_CONFIG_DICT["mqtt_config_ruxin"]
-MQTT_CONFIG = PROJECT_CONFIG_DICT["mqtt_config_eduroam"]
+# MQTT_CONFIG = PROJECT_CONFIG_DICT["mqtt_config_eduroam"]
+MQTT_CONFIG = PROJECT_CONFIG_DICT["mqtt_config_chaosnet"]
 # MQTT_CONFIG = PROJECT_CONFIG_DICT["mqtt_config_local"]
 BROKER = MQTT_CONFIG["broker"]
 MQTT_PORT = MQTT_CONFIG["port"]
