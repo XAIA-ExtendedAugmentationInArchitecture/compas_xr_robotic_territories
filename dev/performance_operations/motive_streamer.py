16cb50973b292d578bc2553afa9c237ffc5ea601
Joseph Kenny <jckenny59@gmail.com>
2025-11-11 11:59:44 -0500

    finish pre study tests and prepare new study name

--- dev/performance_operations/motive_streamer.py
+++ dev/performance_operations/motive_streamer.py
@@ -91,13 +91,11 @@ OPTITRACK_INFO_DICT = PROJECT_CONFIG_DICT.get("optitrack_info", {})
 
 # SESION_DIR_NAME = "20251110_911324_user_study_usermimic"
 
-#TODO: Comment in for Soulaf
-SESION_DIR_NAME = "20251110_0874_user_study"
+SESION_DIR_NAME = "20251110_0874_user_study_inference"
 
-#TODO: Comment in for Lucia
 # SESION_DIR_NAME = "20251110_2022653_user_study"
 
-SESION_DIR_NAME = "20251111_Test_Session"
+# SESION_DIR_NAME = "20251111_Test_Session"
 
 #Geometry Manger Information
 UPLOAD_TO_FIREBASE = PROJECT_CONFIG_DICT.get("upload_geo_to_firebase", False)
