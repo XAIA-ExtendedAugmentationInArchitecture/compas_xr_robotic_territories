560807334d2e0623e65dd8892d218e31b6620d92
Joseph Kenny <jckenny59@gmail.com>
2025-11-12 17:54:25 -0500

    prepare code for user 5

--- dev/performance_operations/motive_streamer.py
+++ dev/performance_operations/motive_streamer.py
@@ -95,9 +95,13 @@ OPTITRACK_INFO_DICT = PROJECT_CONFIG_DICT.get("optitrack_info", {})
 
 # SESION_DIR_NAME = "20251110_2022653_user_study_realtime_mimic"
 # SESION_DIR_NAME = "20251110_2022653_user_study_inference"
-SESION_DIR_NAME = "20251110_2022653_user_study_mimic"
+# SESION_DIR_NAME = "20251110_2022653_user_study_mimic"
 
-# SESION_DIR_NAME = "20251111_Test_Session_reset_2"
+SESION_DIR_NAME = "20251112_123087101_user_study_mimic"
+# SESION_DIR_NAME = "20251112_123087101_user_study_realtime_mimic"
+# SESION_DIR_NAME = "20251112_123087101_user_study_inference"
+
+SESION_DIR_NAME = "20251112_Test_Session_reset"
 
 #Geometry Manger Information
 UPLOAD_TO_FIREBASE = PROJECT_CONFIG_DICT.get("upload_geo_to_firebase", False)
