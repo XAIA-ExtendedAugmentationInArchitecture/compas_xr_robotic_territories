621636b3ffe21a50a752fd943338992913616a5b
Joseph Kenny <jckenny59@gmail.com>
2025-11-12 18:01:59 -0500

    test prior to study

--- dev/performance_operations/motive_streamer.py
+++ dev/performance_operations/motive_streamer.py
@@ -101,7 +101,7 @@ SESION_DIR_NAME = "20251112_123087101_user_study_mimic"
 # SESION_DIR_NAME = "20251112_123087101_user_study_realtime_mimic"
 # SESION_DIR_NAME = "20251112_123087101_user_study_inference"
 
-SESION_DIR_NAME = "20251112_Test_Session_reset"
+# SESION_DIR_NAME = "20251112_Test_Session_reset"
 
 #Geometry Manger Information
 UPLOAD_TO_FIREBASE = PROJECT_CONFIG_DICT.get("upload_geo_to_firebase", False)
