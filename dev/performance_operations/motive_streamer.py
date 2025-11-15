eb63e9336a26cabf147dbdefb7aaf525eaff9cb8
Joseph Kenny <jckenny59@gmail.com>
2025-11-15 14:23:45 -0500

    commit user record

--- dev/performance_operations/motive_streamer.py
+++ dev/performance_operations/motive_streamer.py
@@ -113,9 +113,9 @@ OPTITRACK_INFO_DICT = PROJECT_CONFIG_DICT.get("optitrack_info", {})
 # SESION_DIR_NAME = "20251114_123456_user_study_realtime_mimic"
 # SESION_DIR_NAME = "20251114_123456_user_study_inference"
 
-SESION_DIR_NAME = "20251115_437728_user_study_realtime_mimic"
+# SESION_DIR_NAME = "20251115_437728_user_study_realtime_mimic"
 # SESION_DIR_NAME = "20251115_437728_user_study_inference"
-# SESION_DIR_NAME = "20251115_437728_user_study_mimic"
+SESION_DIR_NAME = "20251115_437728_user_study_mimic"
 
 # SESION_DIR_NAME = "20251115_0649_user_study_realtime_mimic"
 # SESION_DIR_NAME = "20251115_0649_user_study_inference"
