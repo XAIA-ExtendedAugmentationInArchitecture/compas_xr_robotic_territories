d69a91addefb33c36267aced2d12aa74e0a52f5d
Joseph Kenny <jckenny59@gmail.com>
2025-11-25 20:17:09 -0500

    random test for photoshoot with keity

--- dev/performance_operations/motive_streamer.py
+++ dev/performance_operations/motive_streamer.py
@@ -123,9 +123,9 @@ OPTITRACK_INFO_DICT = PROJECT_CONFIG_DICT.get("optitrack_info", {})
 
 # SESION_DIR_NAME = "20251115_0000_user_study_mimic"
 # SESION_DIR_NAME = "20251115_0000_user_study_realtime_mimic"
-SESION_DIR_NAME = "20251115_0000_user_study_inference"
+# SESION_DIR_NAME = "20251115_0000_user_study_inference"
 
-# SESION_DIR_NAME = "20251115_Test_Session_reset_final"
+SESION_DIR_NAME = "20251125_Test_Session_reset_final"
 
 #Geometry Manger Information
 UPLOAD_TO_FIREBASE = PROJECT_CONFIG_DICT.get("upload_geo_to_firebase", False)
